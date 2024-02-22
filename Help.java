2eb2ef7f5c23c3781dfab15ca00f94d178df5119
Alena Antanovich <elenakorogod1992@gmail.com>
2024-02-22 17:54:17 +0700

    updated programm code in file Help.java, file .DS_Store appeared again, so I let it stay, the Help.class file has updated by itself

--- Help.java
+++ Help.java
@@ -7,17 +7,27 @@ class Help {
     public static void main(String args[])
         throws java.io.IOException {
 
-        char choice;
+        char choice, ignore;
 
-        System.out.println("Help:");
-        System.out.println("   1. if");
-        System.out.println("   2. switch");
-        System.out.println("Select the instruction: ");
-        choice = (char) System.in.read();
+        do {
+            System.out.println("Help:");
+            System.out.println("   1. if");
+            System.out.println("   2. switch");
+            System.out.println("   3. for");
+            System.out.println("   4. while");
+            System.out.println("   5. do-while\n");
+            System.out.println("Select the instruction: ");
+
+            choice = (char) System.in.read();
+
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+        } while( choice < '1' | choice > '5');
 
         System.out.println("\n");
 
-        switch(choice) {
+        switch (choice) {
             case '1':
                 System.out.println("The instruction if:\n");
                 System.out.println("if(condition) instruction;");
@@ -31,6 +41,21 @@ class Help {
                 System.out.println("        break;");
                 System.out.println("      // ...");
                 System.out.println("}");
+                break;
+            case '3':
+                System.out.println("The instruction for:\n");
+                System.out.print("for(initialization; condition; iteration)");
+                System.out.println(" instruction;");
+                break;
+            case '4':
+                System.out.println("The instruction while:\n");
+                System.out.println("while(condition) instruction;");
+                break;
+            case '5':
+                System.out.println("The instruction do-while:\n");
+                System.out.println("do {");
+                System.out.println(" instruction;");
+                System.out.println("} while (condition);");
                 break;
             default:
                 System.out.println("Selection was not founded.");
